e16e405b66b1f796fb7d730eea31bc64c017df87
Nithish kumar <87639053+nithish-27@users.noreply.github.com>
2021-09-10 13:29:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -53,6 +53,13 @@
             <input type="text" class="form-control" id="phone" name="phone" aria-describedby="phoneHelp" >
             <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
         </div>
+        <div class="custom-file">
+            <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar" >
+            <label class="custom-file-label" for="avatar">Choose File</label>
+            <small id="avatar" class="form-text text-danger">File Upload is Optional</small>
+
+        </div>
+        
         <br/>
         
         
